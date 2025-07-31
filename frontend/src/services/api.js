6a9a103978e195ac9b02c195cb0926432c87a055
Aastha-Jaiman <aasthajaiman2005@gmail.com>
2025-07-31 14:39:54 +0530

    url3/aastha

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "https://eventregistration-yx8b.onrender.com/api/",
-  baseURL: "hhttps://eventregistration-main.onrender.com/api/",
+  baseURL: "https://eventregistration-main.onrender.com/api/",
   withCredentials: true,
 });
 
